511bd9127cbad21f79e16b35fd7ba72db54de1ba
hyoungsun <hyoungsun@uchicago.edu>
2017-11-03 15:43:55 -0500

    1.0.0

--- MorseCode/Package.swift
+++ MorseCode/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "MorseCode",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/uchicago-codes/mpcs51043-2017-autumn-assignment-5b-hyoungsp", from: "2.0.0"),
+        .package(url: "https://github.com/uchicago-codes/mpcs51043-2017-autumn-assignment-5b-hyoungsp", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
