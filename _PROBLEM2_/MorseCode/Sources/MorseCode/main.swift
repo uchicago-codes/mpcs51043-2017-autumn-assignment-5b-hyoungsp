530d319cb799d54c15d0853cd3673933f59839ff
hyoungsun <hyoungsun@uchicago.edu>
2017-11-04 14:34:50 -0500

    Swift Package Manager
    Library: MorseCoder
    Executable: MorseCode

--- _PROBLEM2_/MorseCode/Sources/MorseCode/main.swift
+++ _PROBLEM2_/MorseCode/Sources/MorseCode/main.swift
@@ -5,3 +5,7 @@ var morseCoder = MorseCoder()
 let myMsg = "hello world"
 
 print(morseCoder.encodeMsg(yourMsg: myMsg))
+
+let yourMsg = ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
+
+print(morseCoder.decodeMsg(yourMsg: yourMsg))
